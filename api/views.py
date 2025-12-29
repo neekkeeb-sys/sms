f33ed71e3ee58b0cc44418c5cb2fedcdaac74670
nbalami <neekkeeb@gmail.com>
2025-12-29 13:13:07 +0545

    add project word

--- api/views.py
+++ api/views.py
@@ -4,5 +4,5 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def test_api(request):
     return Response({
-        "message": "Hello, my name is nigita balami. this is my first Django API"
+        "message": "Hello, my name is nigita balami. this is my first Django  project API"
     })
